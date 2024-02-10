42eec38943634f0f12a7b099b51d617a900b43a5
peach_Jelly <eunjung77819@gmail.com>
2024-02-10 03:19:30 -0500

    first

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        UGAHacks9
+      </h1>
+    </div>
+  );
+}
+
+export default App;
